366719908839013644e5fcbec9d7351f48be2739
thangnt294 <thangnt294@gmail.com>
2023-05-11 18:42:10 +1200

    add port

--- constants.js
+++ constants.js
@@ -7,4 +7,4 @@ export const FB_VERIFY_TOKEN = process.env.FB_VERIFY_TOKEN;
 export const FB_API_VERSION = process.env.FB_API_VERSION;
 export const OPENAI_ORG_ID = process.env.OPENAI_ORG_ID;
 export const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
-export const PORT = process.env.PORT || 1337;
+export const PORT = process.env.PORT || 3000;
